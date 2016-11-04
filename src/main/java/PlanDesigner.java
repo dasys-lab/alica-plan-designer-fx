d316474488951f20be167b273394dd3f7092adbc
Marcel Hesse <hesse@student.uni-kassel.de>
2016-11-04 13:29:17 +0100

    Added JavaFX GUI, all still wip

---
+++ src/main/java/PlanDesigner.java
@@ -0,0 +1,42 @@
+/**
+ * Created by marci on 16.10.16.
+ */
+
+import controller.MainController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class PlanDesigner extends Application {
+
+    private MainController mainController;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        mainController = new MainController();
+        mainController.initialize(null, null);
+        Parent root = FXMLLoader.load(getClass().getResource("test.fxml"));
+        primaryStage.setTitle("Carpe Noctem Plan Designer");
+        primaryStage.setScene(new Scene(root));
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+        primaryStage.show();
+    }
+}
