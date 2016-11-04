0c067fc8f8e7ba17f72f8f6793d89861250a2650
Marcel Hesse <hesse@student.uni-kassel.de>
2016-11-04 16:45:10 +0100

    Added old data model of the plan designer and began using multi module structure

---
+++ alica-plan-designer-fx-ui/src/main/java/de/uni_kassel/vs/cn/planDesigner/controller/MainController.java
@@ -0,0 +1,55 @@
+package de.uni_kassel.vs.cn.planDesigner.controller;
+
+import de.uni_kassel.vs.cn.planDesigner.configuration.Configuration;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import de.uni_kassel.vs.cn.planDesigner.ui.PLDFileTreeView;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by marci on 16.10.16.
+ */
+public class MainController implements Initializable{
+    @FXML
+    private TreeView<File> fileTreeView;
+
+    @FXML
+    private TabPane propertyAndStatusTabPane;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            TreeItem treeItem = new FileTreeItem();
+            treeItem.setExpanded(true);
+            fileTreeView = new PLDFileTreeView(treeItem);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        propertyAndStatusTabPane = new TabPane();
+    }
+
+    private static class FileTreeItem extends TreeItem<File> {
+        private ObservableList<TreeItem<File>> children;
+
+        private Configuration configuration;
+
+        public FileTreeItem() throws URISyntaxException {
+            super(new File("/home/marci/cnws/src/cnc-msl/etc"));
+            configuration = new Configuration();
+            getChildren().addAll(new TreeItem<File>(configuration.getPlansPath().toFile()),
+                    new TreeItem<File>(configuration.getRolesPath().toFile()),
+                    new TreeItem<File>(configuration.getExpressionValidatorsPath().toFile()),
+                    new TreeItem<File>(configuration.getMiscPath().toFile()));
+        }
+    }
+}
---
+++ alica-plan-designer-fx-ui/src/main/java/de/uni_kassel/vs/cn/planDesigner/ui/PLDFileTreeView.java
@@ -0,0 +1,21 @@
+package de.uni_kassel.vs.cn.planDesigner.ui;
+
+import de.uni_kassel.vs.cn.planDesigner.configuration.Configuration;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+
+/**
+ * Created by marci on 16.10.16.
+ */
+public final class PLDFileTreeView extends TreeView<File> {
+    private static final Configuration configuration = new Configuration();
+
+    public PLDFileTreeView(TreeItem<File> fileTreeItem) throws MalformedURLException, URISyntaxException {
+        super(fileTreeItem);
+    }
+
+}
