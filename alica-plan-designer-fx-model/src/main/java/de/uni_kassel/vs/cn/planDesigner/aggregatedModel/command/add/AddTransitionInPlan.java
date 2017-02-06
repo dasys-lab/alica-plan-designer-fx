98d12f69f46be4ea9da12f707eac8170bd992192
Marcel Hesse <marcel-hesse@hotmail.de>
2017-02-06 01:04:45 +0100

    - Added working transition tool

--- alica-plan-designer-fx-model/src/main/java/de/uni_kassel/vs/cn/planDesigner/aggregatedModel/command/add/AddTransitionInPlan.java
+++ alica-plan-designer-fx-model/src/main/java/de/uni_kassel/vs/cn/planDesigner/aggregatedModel/command/add/AddTransitionInPlan.java
@@ -1,7 +1,55 @@
 package de.uni_kassel.vs.cn.planDesigner.aggregatedModel.command.add;
 
+import de.uni_kassel.vs.cn.planDesigner.aggregatedModel.PlanModelVisualisationObject;
+import de.uni_kassel.vs.cn.planDesigner.aggregatedModel.command.Command;
+import de.uni_kassel.vs.cn.planDesigner.alica.State;
+import de.uni_kassel.vs.cn.planDesigner.alica.Transition;
+import de.uni_kassel.vs.cn.planDesigner.pmlextension.uiextensionmodel.PmlUiExtension;
+
+import static de.uni_kassel.vs.cn.planDesigner.alica.xml.EMFModelUtils.getAlicaFactory;
+import static de.uni_kassel.vs.cn.planDesigner.alica.xml.EMFModelUtils.getPmlUiExtensionModelFactory;
+
 /**
  * Created by marci on 02.12.16.
  */
-public class AddTransitionInPlan {
+public class AddTransitionInPlan extends Command<Transition> {
+    private final PmlUiExtension newlyCreatedPmlUiExtension;
+    private PlanModelVisualisationObject parentOfElement;
+    private State from;
+    private State to;
+
+    public AddTransitionInPlan(PlanModelVisualisationObject parentOfElement, State from, State to) {
+        super(getAlicaFactory().createTransition());
+        this.parentOfElement = parentOfElement;
+        this.from = from;
+        this.to = to;
+        this.newlyCreatedPmlUiExtension = getPmlUiExtensionModelFactory().createPmlUiExtension();
+    }
+
+    @Override
+    public void doCommand() {
+        getElementToEdit().setInState(from);
+        getElementToEdit().setOutState(to);
+        parentOfElement.getPlan().getTransitions().add(getElementToEdit());
+        parentOfElement
+                .getPmlUiExtensionMap()
+                .getExtension()
+                .put(getElementToEdit(), newlyCreatedPmlUiExtension);
+    }
+
+    @Override
+    public void undoCommand() {
+        getElementToEdit().setOutState(null);
+        getElementToEdit().setInState(null);
+        parentOfElement.getPlan().getTransitions().remove(getElementToEdit());
+        parentOfElement
+                .getPmlUiExtensionMap()
+                .getExtension()
+                .remove(getElementToEdit());
+    }
+
+    @Override
+    public String getCommandString() {
+        return "Add new transition";
+    }
 }
--- alica-plan-designer-fx-ui/src/main/java/de/uni_kassel/vs/cn/planDesigner/ui/editor/tools/TransitionTool.java
+++ alica-plan-designer-fx-ui/src/main/java/de/uni_kassel/vs/cn/planDesigner/ui/editor/tools/TransitionTool.java
@@ -1,12 +1,18 @@
 package de.uni_kassel.vs.cn.planDesigner.ui.editor.tools;
 
+import de.uni_kassel.vs.cn.planDesigner.aggregatedModel.command.Command;
+import de.uni_kassel.vs.cn.planDesigner.aggregatedModel.command.add.AddTransitionInPlan;
 import de.uni_kassel.vs.cn.planDesigner.alica.Transition;
+import de.uni_kassel.vs.cn.planDesigner.controller.MainController;
 import de.uni_kassel.vs.cn.planDesigner.ui.editor.PlanEditorPane;
 import de.uni_kassel.vs.cn.planDesigner.ui.editor.PlanTab;
+import de.uni_kassel.vs.cn.planDesigner.ui.editor.container.StateContainer;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.event.EventType;
+import javafx.scene.Node;
 import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,6 +26,8 @@ public class TransitionTool extends Tool<Transition> {
 
     private HashMap<EventType, EventHandler> eventHandlerMap = new HashMap<>();
     private boolean initial = true;
+    private StateContainer start;
+    private StateContainer finish;
 
     public TransitionTool(TabPane workbench) {
         super(workbench);
@@ -38,7 +46,24 @@ public class TransitionTool extends Tool<Transition> {
     @Override
     protected Map<EventType, EventHandler> toolRequiredHandlers() {
         if (eventHandlerMap.isEmpty()) {
-
+            eventHandlerMap.put(MouseEvent.MOUSE_CLICKED, event -> {
+                if(((Node)event.getTarget()).getParent() instanceof StateContainer) {
+                    if (initial) {
+                        start = (StateContainer) ((Node)event.getTarget()).getParent();
+                        initial = false;
+                    } else {
+                        finish = (StateContainer) ((Node)event.getTarget()).getParent();
+                        initial = true;
+                        AddTransitionInPlan command = new AddTransitionInPlan(
+                                ((PlanTab)workbench.getSelectionModel().getSelectedItem()).getPlanEditorPane().getPlanModelVisualisationObject(),
+                                start.getContainedElement(), finish.getContainedElement());
+                        MainController.getInstance()
+                                .getCommandStack()
+                                .storeAndExecute(command);
+                        endPhase();
+                    }
+                }
+            });
         }
         return eventHandlerMap;
     }
