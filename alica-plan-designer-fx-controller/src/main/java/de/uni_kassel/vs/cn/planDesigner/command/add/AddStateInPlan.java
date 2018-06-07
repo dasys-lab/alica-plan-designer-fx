4a475ec8a670a3fa4fe19d391eaa28e4819ca8d9
Stefan Jakob <jakob@vs.uni-kassel.de>
2018-06-07 10:03:38 +0200

    minor changes

--- alica-plan-designer-fx-controller/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/add/AddStateInPlan.java
+++ alica-plan-designer-fx-controller/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/add/AddStateInPlan.java
@@ -1,18 +1,14 @@
 package de.uni_kassel.vs.cn.planDesigner.command.add;
 
-import de.uni_kassel.vs.cn.planDesigner.aggregatedModel.PlanModelVisualisationObject;
 import de.uni_kassel.vs.cn.planDesigner.alicamodel.State;
 import de.uni_kassel.vs.cn.planDesigner.command.AbstractCommand;
-import de.uni_kassel.vs.cn.planDesigner.pmlextension.uiextensionmodel.PmlUiExtension;
+import de.uni_kassel.vs.cn.planDesigner.uiextensionmodel.PmlUiExtension;
 
-import static de.uni_kassel.vs.cn.generator.EMFModelUtils.getPmlUiExtensionModelFactory;
 
-/**
- * Created by marci on 02.12.16.
- */
 public class AddStateInPlan extends AbstractCommand<State> {
 
     public static final String DEFAULT_STATE_NAME = "MISSING NAME";
+
     private PlanModelVisualisationObject parentOfElement;
     private PmlUiExtension newlyCreatedPmlUiExtension;
 
--- alica-plan-designer-fx-controller/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/delete/DeleteEntryPointInPlan.java
+++ alica-plan-designer-fx-controller/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/delete/DeleteEntryPointInPlan.java
@@ -9,24 +9,21 @@ import de.uni_kassel.vs.cn.planDesigner.uiextensionmodel.PmlUiExtension;
 public class DeleteEntryPointInPlan extends AbstractCommand {
 
     private State associatedState;
-    // was mapping a plan to its pmlex
-//  private final PlanModelVisualisationObject parentOfElement;
+    private final PlanModelVisualisationObject parentOfElement;
     private Plan parentOfElement;
     private PmlUiExtension pmlUiExtension;
 
     public DeleteEntryPointInPlan(EntryPoint element, Plan parentOfElement) {
         super(element, parentOfElement);
         this.parentOfElement = parentOfElement;
-        //TODO needs to be replaced since its emf
-        //this.pmlUiExtension = parentOfElement.getPmlUiExtensionMap().getExtension().get(getElementToEdit());
+        this.pmlUiExtension = parentOfElement.getPmlUiExtensionMap().getExtension().get(getElementToEdit());
         this.associatedState = element.getState();
     }
 
     @Override
     public void doCommand() {
         parentOfElement.getEntryPoints().remove(getElementToEdit());
-        //TODO needs to be replaced since its emf
-        //parentOfElement.getPmlUiExtensionMap().getExtension().remove(pmlUiExtension);
+        parentOfElement.getPmlUiExtensionMap().getExtension().remove(pmlUiExtension);
         if (associatedState != null) {
             associatedState.setEntryPoint(null);
         }
