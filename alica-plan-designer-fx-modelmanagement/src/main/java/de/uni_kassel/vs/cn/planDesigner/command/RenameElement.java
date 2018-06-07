8b332d3dd02e3098cea909ae2d7da037c2f1a073
Stefan Jakob <jakob@vs.uni-kassel.de>
2018-06-07 14:42:05 +0200

    fixed RenameElement

--- alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/RenameElement.java
+++ alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/RenameElement.java
@@ -1,28 +1,28 @@
 package de.uni_kassel.vs.cn.planDesigner.command;
 
 import de.uni_kassel.vs.cn.planDesigner.alicamodel.PlanElement;
+import de.uni_kassel.vs.cn.planDesigner.modelmanagement.ModelManager;
 
-/**
- * Created by marci on 02.12.16.
- */
-public class RenameElement extends AbstractCommand<PlanElement> {
+public class RenameElement extends AbstractCommand {
     private String newName;
     private String oldName;
+    private PlanElement element;
 
-    public RenameElement(PlanElement element, String newName) {
-        super(element, (PlanElement) element.eResource().getContents().get(0));
+    public RenameElement(ModelManager manager, PlanElement element, String newName) {
+        super(manager);
         this.newName = newName;
-        oldName = getElementToEdit().getName();
+        oldName = element.getName();
+        this.element = element;
     }
 
     @Override
     public void doCommand() {
-        getElementToEdit().setName(newName);
+        element.setName(newName);
     }
 
     @Override
     public void undoCommand() {
-        getElementToEdit().setName(oldName);
+        element.setName(oldName);
     }
 
     @Override
