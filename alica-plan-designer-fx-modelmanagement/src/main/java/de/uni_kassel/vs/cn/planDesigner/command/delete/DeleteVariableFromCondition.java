20ff0747debd1eec59844453fc11a1fc9d914334
Stephan Opfer <opfer@vs.uni-kassel.de>
2018-06-07 15:19:06 +0200

    DeleteVariableFromCondition

--- alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/delete/DeleteVariableFromCondition.java
+++ alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/delete/DeleteVariableFromCondition.java
@@ -1,31 +1,33 @@
 package de.uni_kassel.vs.cn.planDesigner.command.delete;
 
 import de.uni_kassel.vs.cn.planDesigner.alicamodel.Condition;
-import de.uni_kassel.vs.cn.planDesigner.alicamodel.Plan;
 import de.uni_kassel.vs.cn.planDesigner.alicamodel.Variable;
 import de.uni_kassel.vs.cn.planDesigner.command.AbstractCommand;
+import de.uni_kassel.vs.cn.planDesigner.modelmanagement.ModelManager;
 
 public class DeleteVariableFromCondition extends AbstractCommand {
 
-    private final Condition parentOfElement;
+    protected Condition condition;
+    protected Variable variable;
 
-    public DeleteVariableFromCondition(Variable toDelete, Condition parentOfElement, Plan affectedPlan) {
-        super(toDelete, affectedPlan);
-        this.parentOfElement = parentOfElement;
+    public DeleteVariableFromCondition(ModelManager modelManager, Variable variable, Condition condition) {
+        super(modelManager);
+        this.variable = variable;
+        this.condition = condition;
     }
 
     @Override
     public void doCommand() {
-        parentOfElement.getVariables().remove(getElementToEdit());
+        condition.getVariables().remove(variable);
     }
 
     @Override
     public void undoCommand() {
-        parentOfElement.getVariables().add((Variable)getElementToEdit());
+        condition.getVariables().add(variable);
     }
 
     @Override
     public String getCommandString() {
-        return "Remove Variable " + getElementToEdit().getName() + " from " + parentOfElement.getName();
+        return "Remove Variable " + variable.getName() + " from " + condition.getName();
     }
 }
