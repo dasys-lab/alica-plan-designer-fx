0be3e1160446d615085f02f6db3fc3421704b8d2
Stephan Opfer <opfer@vs.uni-kassel.de>
2018-06-07 11:41:32 +0200

    fixed DeleteTaskFromRepository

--- alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/delete/DeleteTaskFromRepository.java
+++ alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/delete/DeleteTaskFromRepository.java
@@ -3,30 +3,30 @@ package de.uni_kassel.vs.cn.planDesigner.command.delete;
 import de.uni_kassel.vs.cn.planDesigner.alicamodel.Task;
 import de.uni_kassel.vs.cn.planDesigner.alicamodel.TaskRepository;
 import de.uni_kassel.vs.cn.planDesigner.command.AbstractCommand;
+import de.uni_kassel.vs.cn.planDesigner.modelmanagement.ModelManager;
 
-/**
- * Created by marci on 01.03.17.
- */
-public class DeleteTaskFromRepository extends AbstractCommand<Task> {
-    private final TaskRepository parentOfElement;
+public class DeleteTaskFromRepository extends AbstractCommand {
+    protected final TaskRepository taskRepository;
+    protected Task task;
 
-    public DeleteTaskFromRepository(TaskRepository parentOfElement, Task toDelete) {
-        super(toDelete, parentOfElement);
-        this.parentOfElement = parentOfElement;
+    public DeleteTaskFromRepository(ModelManager modelManager, TaskRepository taskRepository, Task task) {
+        super(modelManager);
+        this.taskRepository = taskRepository;
+        this.task = task;
     }
 
     @Override
     public void doCommand() {
-        parentOfElement.getTasks().remove(getElementToEdit());
+        taskRepository.getTasks().remove(task);
     }
 
     @Override
     public void undoCommand() {
-        parentOfElement.getTasks().add(getElementToEdit());
+        taskRepository.getTasks().add(task);
     }
 
     @Override
     public String getCommandString() {
-        return "Remove Task " + getElementToEdit().getName() + "from Repository";
+        return "Remove Task " + task.getName() + "from Repository";
     }
 }
