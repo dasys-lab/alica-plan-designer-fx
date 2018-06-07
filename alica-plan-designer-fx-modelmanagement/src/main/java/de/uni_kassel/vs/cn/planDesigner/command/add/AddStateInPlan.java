e2b758a5d42626b2871eafd8fc8101a8204d4d8e
Stefan Jakob <jakob@vs.uni-kassel.de>
2018-06-07 14:48:35 +0200

    fixed AddStateInPlan

--- alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/add/AddStateInPlan.java
+++ alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/add/AddStateInPlan.java
@@ -2,44 +2,48 @@ package de.uni_kassel.vs.cn.planDesigner.command.add;
 
 import de.uni_kassel.vs.cn.planDesigner.alicamodel.State;
 import de.uni_kassel.vs.cn.planDesigner.command.AbstractCommand;
+import de.uni_kassel.vs.cn.planDesigner.modelmanagement.ModelManager;
+import de.uni_kassel.vs.cn.planDesigner.uiextensionmodel.PlanModelVisualisationObject;
 import de.uni_kassel.vs.cn.planDesigner.uiextensionmodel.PmlUiExtension;
 
 
-public class AddStateInPlan extends AbstractCommand<State> {
+public class AddStateInPlan extends AbstractCommand {
 
     public static final String DEFAULT_STATE_NAME = "MISSING NAME";
 
     private PlanModelVisualisationObject parentOfElement;
     private PmlUiExtension newlyCreatedPmlUiExtension;
+    private State newState;
 
-    public AddStateInPlan(PlanModelVisualisationObject parentOfElement, State newObject) {
-        super(newObject, parentOfElement.getPlan());
-        getElementToEdit().setName(DEFAULT_STATE_NAME);
+    public AddStateInPlan(ModelManager manager, PlanModelVisualisationObject parentOfElement, State newState, PmlUiExtension pmlUiExtension) {
+        super(manager);
+        this.newState = newState;
+        newState.setName(DEFAULT_STATE_NAME);
         this.parentOfElement = parentOfElement;
-        this.newlyCreatedPmlUiExtension = getPmlUiExtensionModelFactory().createPmlUiExtension();
+        this.newlyCreatedPmlUiExtension = pmlUiExtension;
     }
 
     @Override
     public void doCommand() {
-        parentOfElement.getPlan().getStates().add(getElementToEdit());
+        parentOfElement.getPlan().getStates().add(newState);
         parentOfElement
                 .getPmlUiExtensionMap()
                 .getExtension()
-                .put(getElementToEdit(), newlyCreatedPmlUiExtension);
+                .put(newState, newlyCreatedPmlUiExtension);
     }
 
     @Override
     public void undoCommand() {
-        parentOfElement.getPlan().getStates().remove(getElementToEdit());
+        parentOfElement.getPlan().getStates().remove(newState);
         parentOfElement
                 .getPmlUiExtensionMap()
                 .getExtension()
-                .remove(getElementToEdit());
+                .remove(newState);
     }
 
     @Override
     public String getCommandString() {
-        return "Add State to Plan " + getElementToEdit();
+        return "Add State to Plan " + parentOfElement.getPlan().getName();
     }
 
     public PmlUiExtension getNewlyCreatedPmlUiExtension() {
