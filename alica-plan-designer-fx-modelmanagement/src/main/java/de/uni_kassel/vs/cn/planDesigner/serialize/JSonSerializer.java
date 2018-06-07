bdc716d038318ed20dc685864b5f4caa8312aa7a
Stefan Jakob <jakob@vs.uni-kassel.de>
2018-06-07 10:08:59 +0200

    removed set id from serializer

--- alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/serialize/JSonSerializer.java
+++ alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/serialize/JSonSerializer.java
@@ -17,13 +17,11 @@ public class JSonSerializer {
 
         Behaviour behaviour = new Behaviour();
         behaviour.setFrequency(30);
-        behaviour.setId(1);
         behaviour.setComment("Behaviour Comment String");
         behaviour.setName("TestBehaviour");
 
         Variable var = new Variable();
         var.setType("Variable Type String");
-        var.setId(2);
         var.setComment("Variable Comment String");
         var.setName("TestVariable");
         behaviour.getVariables().add(var);
@@ -31,21 +29,18 @@ public class JSonSerializer {
         PreCondition preCondition = new PreCondition();
         preCondition.setEnabled(true);
         preCondition.setPluginName("Plugin Name String");
-        preCondition.setId(3);
         preCondition.setComment("PreCondition Comment String");
         preCondition.setName("TestPreCondition");
         behaviour.setPreCondition(preCondition);
 
         RuntimeCondition runtimeCondition = new RuntimeCondition();
         runtimeCondition.setPluginName("Plugin Name String");
-        runtimeCondition.setId(4);
         runtimeCondition.setComment("RuntimeCondition Comment String");
         runtimeCondition.setName("TestRuntimeCondition");
         behaviour.setRuntimeCondition(runtimeCondition);
 
         PostCondition postCondition = new PostCondition();
         postCondition.setPluginName("Plugin Name String");
-        postCondition.setId(5);
         postCondition.setComment("PostCondition Comment String");
         postCondition.setName("TestPostCondition");
         behaviour.setPostCondition(postCondition);
@@ -55,7 +50,6 @@ public class JSonSerializer {
         CapValue val = new CapValue();
         val.setName("TestCapValue");
         val.setComment("CapValue Comment String");
-        val.setId(6);
 
         Characteristic charac = new Characteristic();
         charac.setName("TestCharacteristic");
@@ -65,7 +59,6 @@ public class JSonSerializer {
         Role role = new Role();
         role.setName("TestRole");
         role.setComment("Role Comment String");
-        role.setId(7);
         role.getCharacteristics().add(charac);
 
         try {
