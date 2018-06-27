1237024a79f086d7002bf749a9256ee9d3895b17
Stephan Opfer <opfer@vs.uni-kassel.de>
2018-06-27 10:52:59 +0200

    fix wrong methods

--- alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/DeletePlan.java
+++ alica-plan-designer-fx-modelmanagement/src/main/java/de/uni_kassel/vs/cn/planDesigner/command/DeletePlan.java
@@ -13,10 +13,9 @@ public class DeletePlan extends AbstractCommand {
 
     public DeletePlan(ModelManager modelManager, ModelModificationQuery mmq) {
         super(modelManager);
-        String planName = extractName(modelManager, mmq);
         if (mmq.getModelElementType().equals(Types.PLAN)) {
            for(Plan currentPlan: modelManager.getPlans()) {
-                if (currentPlan.getName().equals(planName)) {
+                if (currentPlan.getName().equals(mmq.getName())) {
                     plan = currentPlan;
                     break;
                 }
@@ -26,20 +25,11 @@ public class DeletePlan extends AbstractCommand {
 
     @Override
     public void doCommand() {
-        modelManager.removePlan(plan);
+        modelManager.removePlanElement(plan, Types.PLAN, true);
     }
 
     @Override
     public void undoCommand() {
-        modelManager.addPlan(plan);
-    }
-
-    private String extractName(ModelManager modelManager, ModelModificationQuery mmq) {
-        String planName = mmq.getAbsoluteDirectory().replace(modelManager.getPlansPath(), "").replace(".pml", "");
-        int index = planName.lastIndexOf(File.separator);
-        if (index != -1) {
-            planName = planName.substring(index + 1);
-        }
-        return planName;
+        modelManager.addPlanElement(plan, Types.PLAN, true);
     }
 }
