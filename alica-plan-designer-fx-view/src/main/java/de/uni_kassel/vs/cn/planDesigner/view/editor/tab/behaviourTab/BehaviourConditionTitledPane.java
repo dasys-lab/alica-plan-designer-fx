500242d4caab717c422a013ea31ea2220af3aefa
Stephan Opfer <opfer@vs.uni-kassel.de>
2018-07-06 15:00:43 +0200

    - tried to integrate a combo box for the plugin name

--- alica-plan-designer-fx-view/src/main/java/de/uni_kassel/vs/cn/planDesigner/view/editor/tab/behaviourTab/BehaviourConditionTitledPane.java
+++ alica-plan-designer-fx-view/src/main/java/de/uni_kassel/vs/cn/planDesigner/view/editor/tab/behaviourTab/BehaviourConditionTitledPane.java
@@ -8,11 +8,19 @@ import de.uni_kassel.vs.cn.planDesigner.view.model.ConditionViewModel;
 import de.uni_kassel.vs.cn.planDesigner.view.model.QuantifierViewModel;
 import de.uni_kassel.vs.cn.planDesigner.view.model.VariableViewModel;
 import de.uni_kassel.vs.cn.planDesigner.view.properties.PropertiesTable;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TitledPane;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.ComboBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.Callback;
+import javafx.util.StringConverter;
 import javafx.util.converter.DefaultStringConverter;
 import javafx.util.converter.LongStringConverter;
+
+import java.util.ArrayList;
 
 /**
  * TODO: Maybe rename/refactor for general condition representation
@@ -21,23 +29,27 @@ public class BehaviourConditionTitledPane extends TitledPane {
 
     protected ConditionViewModel condition;
     protected I18NRepo i18NRepo;
+    PropertiesTable<ConditionViewModel> propertiesTable;
+    private ObservableList<String> pluginNames;
 
     public BehaviourConditionTitledPane(ConditionViewModel condition, String type) {
         super();
         setText(type);
         i18NRepo = I18NRepo.getInstance();
         this.condition = condition;
+        this.pluginNames = FXCollections.observableArrayList();
 
 
         TabPane tabPane = new TabPane();
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
         Tab propertiesTab = new Tab();
         propertiesTab.setText(i18NRepo.getString("label.caption.properties"));
-        PropertiesTable<ConditionViewModel> propertiesTable = new PropertiesTable<>();
+        propertiesTable = new PropertiesTable<>();
         propertiesTable.addColumn(i18NRepo.getString("label.column.name"), "name", new DefaultStringConverter(), true);
         propertiesTable.addColumn(i18NRepo.getString("label.column.id"), "id", new LongStringConverter(), false);
         propertiesTable.addColumn(i18NRepo.getString("label.column.elementType"), "variableType", new DefaultStringConverter(), true);
         propertiesTable.addColumn(i18NRepo.getString("label.column.comment"), "comment", new DefaultStringConverter(), true);
+        addComboColumn(i18NRepo.getString("label.column.pluginName"), "pluginName", new DefaultStringConverter(), true);
         propertiesTab.setContent(propertiesTable);
 
 
@@ -55,6 +67,30 @@ public class BehaviourConditionTitledPane extends TitledPane {
         tabPane.getTabs().addAll(propertiesTab, variablesTab, quantifierTab);
 
         this.setContent(tabPane);
+    }
+
+    public void addPluginName(String pluginName) {
+        this.pluginNames.add(pluginName);
+    }
+
+    public void removePluginName(String pluginName) {
+        this.pluginNames.remove(pluginName);
+    }
+
+    public void addComboColumn(String title, String propertyName, StringConverter converter, boolean editable) {
+        TableColumn<ConditionViewModel, String> column = new TableColumn(title);
+        column.setCellValueFactory(new PropertyValueFactory<ConditionViewModel, String>(propertyName));
+        Callback<TableColumn<ConditionViewModel, String>, TableCell<ConditionViewModel, String>> defaultComboBoxCellFactory
+                = ComboBoxTableCell.<ConditionViewModel, String>forTableColumn(converter);
+        column.setCellFactory(col -> {
+            ComboBoxTableCell<ConditionViewModel, String> cell = (ComboBoxTableCell<ConditionViewModel, String>) defaultComboBoxCellFactory.call(col);
+            cell.getItems().setAll(this.pluginNames);
+            cell.setEditable(editable);
+            return cell;
+        });
+        propertiesTable.getColumns().add(column);
+        // 24 Pixel * (1 empty row + 1 heading) + 2 pixel for border
+        propertiesTable.setPrefHeight(24 * (1 + 1) + 2);
     }
 
     public void setCondition(ConditionViewModel condition) {
--- alica-plan-designer-fx-view/src/main/java/de/uni_kassel/vs/cn/planDesigner/view/properties/PropertiesTable.java
+++ alica-plan-designer-fx-view/src/main/java/de/uni_kassel/vs/cn/planDesigner/view/properties/PropertiesTable.java
@@ -3,6 +3,7 @@ package de.uni_kassel.vs.cn.planDesigner.view.properties;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Callback;
@@ -11,18 +12,18 @@ import javafx.util.StringConverter;
 public class PropertiesTable<S> extends TableView<S> {
 
     public <T> void addColumn(String title, String propertyName, StringConverter<T> converter, boolean editable) {
-        TableColumn<S,T> column = new TableColumn<S, T>(title);
-        column.setCellValueFactory(new PropertyValueFactory<S,T>(propertyName));
-        Callback<TableColumn<S,T>, TableCell<S, T>> defaultTextFieldCellFactory
-                = TextFieldTableCell.<S,T>forTableColumn(converter);
+        TableColumn<S, T> column = new TableColumn<S, T>(title);
+        column.setCellValueFactory(new PropertyValueFactory<S, T>(propertyName));
+        Callback<TableColumn<S, T>, TableCell<S, T>> defaultTextFieldCellFactory
+                = TextFieldTableCell.<S, T>forTableColumn(converter);
         column.setCellFactory(col -> {
-            TableCell<S,T> cell = defaultTextFieldCellFactory.call(col);
+            TableCell<S, T> cell = defaultTextFieldCellFactory.call(col);
             cell.setEditable(editable);
             return cell;
         });
         getColumns().add(column);
         // 24 Pixel * (1 empty row + 1 heading) + 2 pixel for border
-        setPrefHeight(24 * (1+1) + 2);
+        setPrefHeight(24 * (1 + 1) + 2);
     }
 
     public void addItem(S viewModelItem) {
