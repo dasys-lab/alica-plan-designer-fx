9151ca77b003623990df3405eddf9a4dbc9e49d4
Stephan Opfer <opfer@vs.uni-kassel.de>
2019-01-23 15:05:39 +0100

    fix minor bug: tasrepo is able to be opened from the repositoryview by clicking on tasks

--- alica-plan-designer-fx-view/src/main/java/de/unikassel/vs/alica/planDesigner/view/repo/RepositoryHBox.java
+++ alica-plan-designer-fx-view/src/main/java/de/unikassel/vs/alica/planDesigner/view/repo/RepositoryHBox.java
@@ -2,12 +2,14 @@ package de.unikassel.vs.alica.planDesigner.view.repo;
 
 import de.unikassel.vs.alica.planDesigner.view.menu.RenameElementMenuItem;
 import de.unikassel.vs.alica.planDesigner.view.model.SerializableViewModel;
+import de.unikassel.vs.alica.planDesigner.view.model.TaskViewModel;
 import de.unikassel.vs.alica.planDesigner.view.model.ViewModelElement;
 import de.unikassel.vs.alica.planDesigner.controller.MainWindowController;
 import de.unikassel.vs.alica.planDesigner.handlerinterfaces.IGuiModificationHandler;
 import de.unikassel.vs.alica.planDesigner.view.editor.tools.DraggableHBox;
 import de.unikassel.vs.alica.planDesigner.view.menu.DeleteElementMenuItem;
 import de.unikassel.vs.alica.planDesigner.view.menu.ShowUsagesMenuItem;
+import javafx.concurrent.Task;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.input.MouseButton;
 
@@ -43,8 +45,14 @@ public class RepositoryHBox extends DraggableHBox {
         // double click for open the corresponding file
         setOnMouseClicked(e -> {
             if (e.getButton().equals(MouseButton.PRIMARY) && e.getClickCount() == 2) {
-                // TODO handle double click on tasks which are no serializableViewModel
-                MainWindowController.getInstance().openFile((SerializableViewModel) viewModelElement);
+                if (viewModelElement instanceof SerializableViewModel) {
+                    MainWindowController.getInstance().openFile((SerializableViewModel) viewModelElement);
+                } else if (viewModelElement instanceof TaskViewModel) {
+                    TaskViewModel taskViewModel = (TaskViewModel) viewModelElement;
+                    MainWindowController.getInstance().openFile(taskViewModel.getTaskRepositoryViewModel());
+                } else {
+                    throw new RuntimeException("RepositoryHBox: Unkown ViewModelElement type " + viewModelElement.getType() + " for opening tab!");
+                }
                 e.consume();
             }
         });
