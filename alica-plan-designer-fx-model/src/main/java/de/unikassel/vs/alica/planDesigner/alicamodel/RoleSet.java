0bba0220ed87a9bbae3ad4df2656a261757430cc
Alex Jahl <ajahl.se@gmail.com>
2019-03-28 10:53:54 +0100

    Implementation of - create role set - create role - role set de-/serialization  #55

---
+++ alica-plan-designer-fx-model/src/main/java/de/unikassel/vs/alica/planDesigner/alicamodel/RoleSet.java
@@ -0,0 +1,47 @@
+package de.unikassel.vs.alica.planDesigner.alicamodel;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RoleSet extends SerializablePlanElement {
+
+    protected final ArrayList<Role> roles = new ArrayList<>();
+
+    public RoleSet() {}
+
+    public Role getRole(long roleID) {
+        for (Role role : roles) {
+            if (roleID == role.getId()) {
+                return role;
+            }
+        }
+        return null;
+    }
+    public boolean contains (Role role) {
+        return roles.contains(role);
+    }
+
+    public List<Role> getRoles() {
+        return Collections.unmodifiableList(roles);
+    }
+
+    public void addRole(Role role) {
+        role.registerDirtyFlag(this.changeListenerForDirtyFlag);
+        roles.add(role);
+        this.changeListenerForDirtyFlag.setDirty();
+    }
+
+    public void removeRole(Role role) {
+        roles.remove(role);
+        this.changeListenerForDirtyFlag.setDirty();
+    }
+
+    @Override
+    public void registerDirtyFlag() {
+        super.registerDirtyFlag();
+        for (Role role : roles) {
+            role.registerDirtyFlag(this.changeListenerForDirtyFlag);
+        }
+    }
+}
