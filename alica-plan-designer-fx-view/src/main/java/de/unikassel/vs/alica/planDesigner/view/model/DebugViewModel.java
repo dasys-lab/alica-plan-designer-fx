b0bca4017a3466d1a5dcba02efc421f29bbbb1bc
link9 <xlink9x@web.de>
2019-07-17 08:01:13 +0200

    added an empty debug view

---
+++ alica-plan-designer-fx-view/src/main/java/de/unikassel/vs/alica/planDesigner/view/model/DebugViewModel.java
@@ -0,0 +1,7 @@
+package de.unikassel.vs.alica.planDesigner.view.model;
+
+public class DebugViewModel extends SerializableViewModel {
+    public DebugViewModel(long id, String name, String type) {
+        super(id, name, type);
+    }
+}
