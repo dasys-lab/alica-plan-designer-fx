a84336755756586a1ae94376e25f92c4ffdc40fc
link9 <xlink9x@web.de>
2019-10-17 16:41:50 +0200

    starting ALICA engine

--- alica-plan-designer-fx-view/src/main/java/de/unikassel/vs/alica/planDesigner/view/editor/tab/debugTab/DebugTab.java
+++ alica-plan-designer-fx-view/src/main/java/de/unikassel/vs/alica/planDesigner/view/editor/tab/debugTab/DebugTab.java
@@ -8,6 +8,8 @@ import de.unikassel.vs.alica.planDesigner.view.img.AlicaIcon;
 import de.unikassel.vs.alica.planDesigner.view.model.SerializableViewModel;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
@@ -18,17 +20,36 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 public class DebugTab extends EditorTab {
 
     public static final int SPACING = 10;
     private IAlicaMessageHandler alicaMessageHandler;
     private final VBox allAgentsVBox;
+    private final TextArea debugTextArea;
+    private Process roscoreProcess;
+    private Process pdAlicaRunnerProcess;
+    private final Button runButton;
 
     public DebugTab(SerializableViewModel serializableViewModel, EditorTabPane editorTabPane, IAlicaMessageHandler handler) {
         super(serializableViewModel, editorTabPane.getGuiModificationHandler());
         alicaMessageHandler = handler;
 
-        TextArea textArea = new TextArea();
+        debugTextArea = new TextArea();
+        debugTextArea.setEditable(false);
+
+        // redirect System.out to the debugTextArea
+        OutputStream stream = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                debugTextArea.appendText(String.valueOf((char) b));
+            }
+        };
+        System.setOut(new PrintStream(stream, true));
 
 //        HBox topic = new HBox();
 //        Label labelTopic = new Label("Topic:");
@@ -63,7 +84,7 @@ public class DebugTab extends EditorTab {
         allAgentsVBox = new VBox(SPACING);
         allAgentsVBox.getChildren().add(addAgentButton);
 
-        Button runButton = new Button("Run");
+        runButton = new Button("Run");
         runButton.setGraphic(new ImageView(new Image(AlicaIcon.class.getClassLoader().getResourceAsStream("images/run16x16.png"))));
         runButton.setMaxWidth(Double.MAX_VALUE);
         runButton.setOnAction(this::onRunClicked);
@@ -72,7 +93,7 @@ public class DebugTab extends EditorTab {
         vBox.getChildren().addAll(allAgentsVBox, runButton);
 
         HBox hbox = new HBox(SPACING);
-        hbox.getChildren().addAll(textArea, vBox);
+        hbox.getChildren().addAll(debugTextArea, vBox);
         HBox.setHgrow(vBox, Priority.ALWAYS);
         //HBox.setHgrow(vbox, Priority.ALWAYS);
 
@@ -106,22 +127,47 @@ public class DebugTab extends EditorTab {
         // Simulates a AlicaEngineInfo message:
         //(senderId = (value = "\x02\x00\x00\x00", type = wildcard), masterPlan = "ServeMaster", currentPlan = "ServeMaster", currentState = "Stop", currentRole = "Assistant", currentTask = "DefaultTask", agentIdsWithMe = [(value = "\x02\x00\x00\x00", type = wildcard)])
 
+        if (pdAlicaRunnerProcess == null || roscoreProcess == null) {
+            try {
+                // start roscore, because we still need it
+                ProcessBuilder pb = new ProcessBuilder("bash", "-c", "echo 'Sourcing setup.bash'; source /opt/ros/melodic/setup.bash; echo 'Starting roscore...'; roscore");
+                pb.environment().put("PATH", pb.environment().get("PATH") + ":/opt/ros/melodic/bin");
+                pb.inheritIO();
+                roscoreProcess = pb.start();
 
-        for (String key :
-                System.getenv().keySet()) {
-            System.out.println(key + ": " + System.getenv().get(key));
+                // Load pd_alica_runner
+                pb = new ProcessBuilder("bash", "-c", "echo Starting pd_alica_runner...; /opt/pd-debug/ttb-ws/devel/lib/pd_alica_runner/pd_alica_runner -m ServeMaster -rd  /opt/pd-debug/cnc-turtlebots/etc/roles/ -r ServiceRobotsRoleSet -sim");
+                pb.directory(new File("/opt/pd-debug/cnc-turtlebots"));
+                pb.environment().put("PATH", pb.environment().getOrDefault("PATH", "") + ":/opt/ros/melodic/bin");
+                pb.environment().put("ROBOT", "donatello");
+                pb.environment().put("LD_LIBRARY_PATH", "/opt/pd-debug/ttb-ws/devel/lib:/opt/ros/melodic/lib" + pb.environment().getOrDefault("LD_LIBRARY_PATH", ""));
+                pb.environment().put("ROS_MASTER_URI", "http://localhost:11311");
+                pb.inheritIO();
+                pdAlicaRunnerProcess = pb.start();
 
-        }
+                // if everything worked, change the run button to a stop button
+                runButton.setText("Stop");
+                runButton.setGraphic(new ImageView(new Image(AlicaIcon.class.getClassLoader().getResourceAsStream("images/stop16x16.png"))));
 
-        try {
+            } catch (Exception e) {
+                System.err.println("Could not load roscore :(");
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                pdAlicaRunnerProcess.destroyForcibly();
+                pdAlicaRunnerProcess.waitFor();
+                roscoreProcess.destroyForcibly();
+                roscoreProcess.waitFor();
 
-            ProcessBuilder pb = new ProcessBuilder("bash", "-c", "roscore")
-                    .inheritIO();
-            pb.environment().put("PATH", "/opt/ros/melodic/bin");
-            pb.start();
-        } catch (Exception e) {
-            System.err.println("Could not load roscore :(");
-            e.printStackTrace();
+                System.out.println("Destroyed both processes.");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // change the stop button to a start button
+            runButton.setText("Run");
+            runButton.setGraphic(new ImageView(new Image(AlicaIcon.class.getClassLoader().getResourceAsStream("images/run16x16.png"))));
         }
 
         /*
@@ -212,4 +258,9 @@ public class DebugTab extends EditorTab {
         this.alicaMessageHandler = alicaMessageHandler;
     }
 
+    @Override
+    public void setOnCloseRequest(EventHandler<Event> value) {
+        super.setOnCloseRequest(value);
+        System.out.println("Closing..........");
+    }
 }
